f8a1a67654d53e20994c4542a8c5c09615301495
suraj0233 <160613987+suraj0233@users.noreply.github.com>
2025-02-10 20:10:25 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -21,46 +21,10 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "spacelift-vm-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "spacelift-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "spacelift-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
 }
